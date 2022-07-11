b41be83f80e7192913379cd6d78c303aa75e6cc5
Matthew Holmes <matthew_holmes@Matthews-MacBook-Pro.local>
2022-07-10 19:20:42 -0700

    add basic pages and functions

--- web_app/urls.py
+++ web_app/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('login', views.login_user, name='login_user'),
+    path('profile', views.profile, name='profile'),
+
 ]
--- web_app/views.py
+++ web_app/views.py
@@ -1,9 +1,31 @@
-
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
+from django.urls import reverse
+from .forms import NewLoginForm
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'web_app/index.html')
+
+
+def login_user(request):
+    if request.method == "GET":
+        return render(request, 'web_app/login.html', {
+            'form': NewLoginForm()
+        })
+    elif request.method == "POST":
+        form = NewLoginForm(request.POST)
+        if form.is_valid():
+            password = form.cleaned_data['password']
+            user = authenticate(
+                request, username=form.cleaned_data['username'], password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse('profile'))
+
+
+def profile(request):
+    return render(request, ('web_app/profile.html'))
